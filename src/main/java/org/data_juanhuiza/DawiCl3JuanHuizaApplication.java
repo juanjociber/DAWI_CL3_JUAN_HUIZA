e1e09dc69e8771673092cfb44b7e780d28e28123
juanjociber <i202015638@cibertec.edu.pe>
2022-06-20 16:40:44 -0500

    Iniciando CL3 Desarrollo de Aplicaciones Web I

---
+++ src/main/java/org/data_juanhuiza/DawiCl3JuanHuizaApplication.java
@@ -0,0 +1,14 @@
+package org.data_juanhuiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3JuanHuizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3JuanHuizaApplication.class, args);
+		System.out.println("Servidor iniciado...");
+	}
+
+}
